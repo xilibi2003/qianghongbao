4f86fdc9b4cb191edf635317baa88064181dc60c
emmett <xilibi2003@gmail.com>
2017-09-12 22:26:11 +0800

    Add youmi

--- app/src/main/java/com/luffy88/qianghongbao/HomeActivity.java
+++ app/src/main/java/com/luffy88/qianghongbao/HomeActivity.java
@@ -9,9 +9,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+
+import net.youmi.android.AdManager;
+import net.youmi.android.nm.bn.BannerManager;
+import net.youmi.android.nm.bn.BannerViewListener;
 
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "HomeActivity";
 
     private Button mServiceBtn;
     private final int MSG_CHECK_SERVICE = 1;
@@ -35,8 +41,34 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        AdManager.getInstance(getApplication()).init(BuildConfig.youmiID, BuildConfig.youmiKEY, false);
+
         mServiceBtn = (Button)findViewById(R.id.button);
         mServiceBtn.setOnClickListener(this);
+
+        // 获取广告条
+        View bannerView = BannerManager.getInstance(getApplication())
+                .getBannerView(this, new BannerViewListener() {
+                    @Override
+                    public void onRequestSuccess() {
+
+                    }
+
+                    @Override
+                    public void onSwitchBanner() {
+
+                    }
+
+                    @Override
+                    public void onRequestFailed() {
+
+                    }
+                });
+
+        // 获取要嵌入广告条的布局
+        LinearLayout bannerLayout = (LinearLayout) findViewById(R.id.ll_banner);
+        // 将广告条加入到布局中
+        bannerLayout.addView(bannerView);
     }
 
 
@@ -44,6 +76,13 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     protected void onResume() {
         super.onResume();
         myHandler.sendEmptyMessageDelayed(MSG_CHECK_SERVICE, 100);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 展示广告条窗口的 onDestroy() 回调方法中调用
+        BannerManager.getInstance(getApplication()).onDestroy();
     }
 
     @Override
